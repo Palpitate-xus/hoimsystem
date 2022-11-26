c884bbb1f7d73220df8645bacda8580cfb5835ef
Palpitate-xus <palpitate.xus@outlook.com>
2022-11-26 17:30:36 +0800

    引入mysql数据库

--- django_be/django_be/settings.py
+++ django_be/django_be/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'django_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hoimsystem',
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 

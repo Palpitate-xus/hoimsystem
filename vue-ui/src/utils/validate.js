11cd55b44c3f07d773006c9ec467d253bda3f9f3
Palpitate-xus <palpitate.xus@outlook.com>
2022-11-26 16:43:16 +0800

    delete something

--- vue-ui/src/utils/validate.js
+++ vue-ui/src/utils/validate.js
@@ -1,5 +1,4 @@
 /**
- * @author chuzhixin 1204505056@qq.com （不想保留author可删除）
  * @description 判读是否为外链
  * @param path
  * @returns {boolean}
@@ -8,7 +7,6 @@ export function isExternal(path) {
   return /^(https?:|mailto:|tel:)/.test(path)
 }
 /**
- * @author https://vue-admin-beautiful.com （不想保留author可删除）
  * @description 校验密码是否小于6位
  * @param str
  * @returns {boolean}
@@ -17,7 +15,6 @@ export function isPassword(str) {
   return str.length >= 6
 }
 /**
- * @author chuzhixin 1204505056@qq.com （不想保留author可删除）
  * @description 判断是否是字符串
  * @param str
  * @returns {boolean}
@@ -26,7 +23,6 @@ export function isString(str) {
   return typeof str === 'string' || str instanceof String
 }
 /**
- * @author chuzhixin 1204505056@qq.com （不想保留author可删除）
  * @description 判断是否是数组
  * @param arg
  * @returns {arg is any[]|boolean}
@@ -38,7 +34,6 @@ export function isArray(arg) {
   return Array.isArray(arg)
 }
 /**
- * @author chuzhixin 1204505056@qq.com （不想保留author可删除）
  * @description 判断是否是手机号
  * @param str
  * @returns {boolean}

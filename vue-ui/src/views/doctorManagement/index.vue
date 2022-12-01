221f1088abb97abbb3bece3a7bef57308b70eb66
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-01 20:43:34 +0800

    add new pages

--- vue-ui/src/views/doctorManagement/index.vue
+++ vue-ui/src/views/doctorManagement/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="test-container">
-    <el-divider content-position="left">你可以在这里写demo</el-divider>
+    <el-divider content-position="left">这是医生管理的页面</el-divider>
   </div>
 </template>
 <script>
   export default {
-    name: 'Test',
+    name: 'Index',
     data() {
       return { show: true }
     },

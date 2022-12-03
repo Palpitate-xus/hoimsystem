b824893c4f342347719ad2526fded5b3c163981c
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-03 21:58:52 +0800

    add test table

--- vue-ui/src/views/prescriptionManagement/index.vue
+++ vue-ui/src/views/prescriptionManagement/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>测试</h1>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
   export default {
     name: 'PresciptionManagement',
+    data() {
+      return {
+        tableData: [
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄',
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+        ],
+      }
+    },
   }
 </script>

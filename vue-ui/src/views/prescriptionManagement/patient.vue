4bc206de5d4da34907c8e0462f738fcca0aba6e4
Palpitate-xus <xus1758011080@outlook.com>
2022-12-03 22:13:51 +0800

    add pages

---
+++ vue-ui/src/views/prescriptionManagement/patient.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PresciptionPatient',
+    data() {
+      return {
+        tableData: [
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄',
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+        ],
+      }
+    },
+  }
+</script>

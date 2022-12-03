6da0a38e22c6064aa2d57067fe1701395a7b1b0e
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-03 21:59:31 +0800

    set theme

--- vue-ui/src/config/theme.config.js
+++ vue-ui/src/config/theme.config.js
@@ -5,9 +5,9 @@ const theme = {
   //是否国定头部 固定fixed 不固定noFixed
   header: 'fixed',
   //横纵布局 horizontal vertical
-  layout: 'vertical',
+  layout: 'horizontal',
   //是否开启主题配置按钮
-  themeBar: true,
+  themeBar: false,
   //是否显示多标签页
   tabsBar: true,
 }

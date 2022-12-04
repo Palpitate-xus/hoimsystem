9f97f07911fc2bf345fda7edcf61437367ae952e
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-04 22:21:25 +0800

    add registration model

--- django_be/hoimsystem/models/patientOp.py
+++ django_be/hoimsystem/models/patientOp.py
@@ -2,3 +2,10 @@ from django.db import models
 import uuid
 from django.db import models
 # Create your models here.
+
+class registration(models.Model):
+  registration_uuid = models.UUIDField()
+  patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)
+  doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
+  category = models.ForeignKey
+  department_id = models.ForeignKey('department', on_delete=models.CASCADE)

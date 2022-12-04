4bc06051c52bb2f8d157d76684f78f31254edac6
Palpitate-xus <xus1758011080@outlook.com>
2022-12-04 22:21:04 +0800

    add department model

--- django_be/hoimsystem/models/adminOp.py
+++ django_be/hoimsystem/models/adminOp.py
@@ -2,3 +2,14 @@ from django.db import models
 import uuid
 from django.db import models
 # Create your models here.
+
+class department(models.Model):
+  department_id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=24)
+  phone = models.CharField(max_length=11)
+  location = models.CharField(max_length=24)
+  director = models.ForeignKey('doctor', on_delete=models.PROTECT)
+
+class reg_category(models.Model):
+  reg_category_id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=10)

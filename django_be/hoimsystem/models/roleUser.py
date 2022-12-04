548a1809b7912817f6e3cfcefb3242b30c89f7e2
Palpitate-xus <xus1758011080@outlook.com>
2022-12-04 21:58:10 +0800

    add roleUser models

--- django_be/hoimsystem/models/roleUser.py
+++ django_be/hoimsystem/models/roleUser.py
@@ -4,4 +4,21 @@ from django.db import models
 # Create your models here.
 
 class role(models.Model):
+  role_id = models.UUIDField(primary_key=True)
+  accessToken = models.CharField(max_length=50)
+
+class patient(models.Model):
+  patient_id = models.AutoField()  # 自动递增的病人id
+  name = models.CharField(max_length=24)  # 病人姓名
+  sex = models.IntegerField(max_length=1)  # 病人性别，0女1男
+  birthday = models.DateField()  # 病人出生日期
+  phone = models.CharField(max_length=11)  # 病人联系方式
+  address = models.CharField(max_length=100)  # 病人住址
+
+class doctor(models.Model):
+  doctor_id = models.AutoField()  # 自动递增的医生id
+  name = models.CharField(max_length=24)  # 医生姓名
+  sex = models.IntegerField(max_length=1)  # 医生性别，0女1男
+  title = models.CharField(max_length=10)  # 医生职称
+  education = models.CharField(max_length=10)  # 医生学历
   

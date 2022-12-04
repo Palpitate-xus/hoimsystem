25d0eeb02e1791e2e035c4ac322db3c90d66e3fb
Palpitate-xus <xus1758011080@outlook.com>
2022-12-04 22:20:54 +0800

    add primary_key

--- django_be/hoimsystem/models/roleUser.py
+++ django_be/hoimsystem/models/roleUser.py
@@ -8,17 +8,19 @@ class role(models.Model):
   accessToken = models.CharField(max_length=50)
 
 class patient(models.Model):
-  patient_id = models.AutoField()  # 自动递增的病人id
+  patient_id = models.AutoField(primary_key=True)  # 自动递增的病人id
   name = models.CharField(max_length=24)  # 病人姓名
   sex = models.IntegerField(max_length=1)  # 病人性别，0女1男
   birthday = models.DateField()  # 病人出生日期
   phone = models.CharField(max_length=11)  # 病人联系方式
+  password = models.CharField(max_length=24)  # 病人密码
   address = models.CharField(max_length=100)  # 病人住址
 
 class doctor(models.Model):
-  doctor_id = models.AutoField()  # 自动递增的医生id
+  doctor_id = models.AutoField(primary_key=True)  # 自动递增的医生id
   name = models.CharField(max_length=24)  # 医生姓名
   sex = models.IntegerField(max_length=1)  # 医生性别，0女1男
   title = models.CharField(max_length=10)  # 医生职称
   education = models.CharField(max_length=10)  # 医生学历
-  
+  phone = models.CharField(max_length=11)  # 医生联系方式
+  password = models.CharField(max_length=24)  # 医生密码

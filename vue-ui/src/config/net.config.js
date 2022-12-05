be07ea33c67d0a66d87e844d89be315642d03e3d
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-05 15:03:09 +0800

    第一次数据库迁移及前后端连接测试

--- vue-ui/src/config/net.config.js
+++ vue-ui/src/config/net.config.js
@@ -5,7 +5,7 @@ const network = {
   // 默认的接口地址 如果是开发环境和生产环境走vab-mock-server，当然你也可以选择自己配置成需要的接口地址
   baseURL:
     process.env.NODE_ENV === 'development'
-      ? 'vab-mock-server'
+      ? 'http://127.0.0.1:8000/api'
       : 'vab-mock-server',
   //配后端数据的接收方式application/json;charset=UTF-8或者application/x-www-form-urlencoded;charset=UTF-8
   contentType: 'application/json;charset=UTF-8',

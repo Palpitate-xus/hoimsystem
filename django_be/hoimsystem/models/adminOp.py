27698b5c3e81c3764e65f2c5bc377c8408faab54
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-05 11:18:35 +0800

    add 预约就诊时间段

--- django_be/hoimsystem/models/adminOp.py
+++ django_be/hoimsystem/models/adminOp.py
@@ -15,3 +15,8 @@ class department(models.Model):
 class reg_category(models.Model):
   reg_category_id = models.AutoField(primary_key=True)  # 挂号类别id
   name = models.CharField(max_length=10)  # 挂号类别名称
+
+# 预约就诊时间段
+class timeslot(models.Model):
+  timeslot_id = models.AutoField(primary_key=True)
+  time = models.CharField(max_length=20)  

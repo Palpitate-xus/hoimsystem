6bb63f2757cbdc89572715fd6ac607ae0b0c48ae
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-05 13:59:05 +0800

    delete comment

--- vue-ui/vue.config.js
+++ vue-ui/vue.config.js
@@ -180,8 +180,6 @@ module.exports = {
       scss: {
         /*sass-loader 8.0语法 */
         //prependData: '@import "~@/styles/variables.scss";',
-
-        /*sass-loader 9.0写法，感谢github用户 shaonialife*/
         additionalData(content, loaderContext) {
           const { resourcePath, rootContext } = loaderContext
           const relativePath = path.relative(rootContext, resourcePath)

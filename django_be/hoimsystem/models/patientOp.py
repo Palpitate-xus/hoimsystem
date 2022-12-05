ad21c99e4a2ec4f7c1f00ec1279bea1e2097c449
Palpitate-xus <xus1758011080@outlook.com>
2022-12-05 11:08:56 +0800

    update models

--- django_be/hoimsystem/models/patientOp.py
+++ django_be/hoimsystem/models/patientOp.py
@@ -3,9 +3,27 @@ import uuid
 from django.db import models
 # Create your models here.
 
+# 挂号
 class registration(models.Model):
   registration_uuid = models.UUIDField()
   patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)
   doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
-  category = models.ForeignKey
+  category = models.ForeignKey('reg_category', on_delete=models.PROTECT)
   department_id = models.ForeignKey('department', on_delete=models.CASCADE)
+  status = models.IntegerField(max_length=1)
+
+# 预约
+class appointment(models.Model):
+  registration_uuid = models.UUIDField()
+  patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)
+  doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
+  category = models.ForeignKey('reg_category', on_delete=models.PROTECT)
+  department_id = models.ForeignKey('department', on_delete=models.CASCADE)
+  prefer_time = models.ForeignKey
+  status = models.IntegerField(max_length=1)
+
+
+# 预约违约记录
+class breach_record(models.Model):
+  breach_id = models.UUIDField()
+  registration_id = models.ForeignKey('registration', on_delete=models.CASCADE)

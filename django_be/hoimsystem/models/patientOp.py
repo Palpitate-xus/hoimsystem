ef2f06fb0b8172c82fbb0d1ea53cb30256641710
Palpitate-xus <xus1758011080@outlook.com>
2022-12-05 11:18:48 +0800

    update registration model

--- django_be/hoimsystem/models/patientOp.py
+++ django_be/hoimsystem/models/patientOp.py
@@ -5,12 +5,13 @@ from django.db import models
 
 # 挂号
 class registration(models.Model):
-  registration_uuid = models.UUIDField()
-  patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)
-  doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
-  category = models.ForeignKey('reg_category', on_delete=models.PROTECT)
-  department_id = models.ForeignKey('department', on_delete=models.CASCADE)
-  status = models.IntegerField(max_length=1)
+  registration_uuid = models.UUIDField()  # 挂号id
+  registration_id = models.IntegerField(max_length=2)  # 就诊顺序
+  patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)  # 病人id
+  doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)  # 医生id
+  category = models.ForeignKey('reg_category', on_delete=models.PROTECT)  # 挂号类别id
+  department_id = models.ForeignKey('department', on_delete=models.CASCADE)  # 科室id
+  status = models.IntegerField(max_length=1)  # 挂号状态（是否就诊）
 
 # 预约
 class appointment(models.Model):

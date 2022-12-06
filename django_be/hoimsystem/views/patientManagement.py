4f5bf46e5b4214341c0b4a7b8a23542cab074934
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 15:02:15 +0800

    add apis

--- django_be/hoimsystem/views/patientManagement.py
+++ django_be/hoimsystem/views/patientManagement.py
@@ -7,3 +7,47 @@ import json
 def test(request):
     response = {"code": 200, "msg": 'success'}
     return HttpResponse(json.dumps(response))
+
+# 病人挂号
+def patient_registration(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+# 挂号记录查看
+def get_registration_list(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+# 病人预约
+def patient_appointment(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+# 预约记录查看
+def get_appointment_list(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+# 检查记录查看
+def get_inspection_list(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+# 病历查看
+def get_medicalrecords_list(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+# 收费记录查看
+def get_charges_list(request):
+    received_data = json.loads(request.body.decode())
+    response = {"code": 200, "msg": 'success'}
+    return HttpResponse(json.dumps(response))
+
+

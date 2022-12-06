889e1a91f49fb3c7e81c2c97afcd94edc11bb0b5
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 16:40:47 +0800

    add apis

--- vue-ui/src/api/doctorManagement.js
+++ vue-ui/src/api/doctorManagement.js
@@ -1,24 +1,8 @@
 import request from '@/utils/request'
 
-export function getList(data) {
+export function getDoctorList(data) {
   return request({
     url: '/doctorManagement/getList',
-    method: 'post',
-    data,
-  })
-}
-
-export function doEdit(data) {
-  return request({
-    url: '/doctorManagement/doEdit',
-    method: 'post',
-    data,
-  })
-}
-
-export function doDelete(data) {
-  return request({
-    url: '/doctorManagement/doDelete',
     method: 'post',
     data,
   })

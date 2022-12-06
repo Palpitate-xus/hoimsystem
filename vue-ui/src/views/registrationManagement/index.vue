fcbc63459df48cf73af3c251fed6f47ea1ce8b6e
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 14:37:33 +0800

    更新病人挂号页面

--- vue-ui/src/views/registrationManagement/index.vue
+++ vue-ui/src/views/registrationManagement/index.vue
@@ -1,40 +1,67 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+    <el-form ref="form" :model="form" label-width="140px">
+      <el-form-item label="科室选择">
+        <el-select v-model="form.region" placeholder="请选择科室">
+          <el-option label="上午" value="shanghai"></el-option>
+          <el-option label="下午" value="beijing"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="即时配送">
+      <el-form-item label="医生姓名">
+        <el-select v-model="form.region" placeholder="请选择医生">
+          <el-option label="上午" value="shanghai"></el-option>
+          <el-option label="下午" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="就诊时间段">
+        <el-select v-model="form.region" placeholder="请选择就诊时间段">
+          <el-option label="上午" value="shanghai"></el-option>
+          <el-option label="下午" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="是否专家号">
         <el-switch v-model="form.delivery"></el-switch>
       </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input v-model="form.desc" type="textarea"></el-input>
-      </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="onSubmit">查找</el-button>
       </el-form-item>
     </el-form>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      :element-loading-text="elementLoadingText"
+    >
+      <el-table-column
+        show-overflow-tooltip
+        prop="id"
+        label="id"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="医生"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="时间段"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="类别"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="email"
+        label="剩余号源数量"
+      ></el-table-column>
+      <el-table-column show-overflow-tooltip label="操作" width="200">
+        <template #default="{ row }">
+          <el-button type="text" @click="handleEdit(row)">挂号</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 

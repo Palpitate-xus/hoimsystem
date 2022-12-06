8296cc2be7bf206c47a5317e680463421931be3f
Palpitate-xus <xus1758011080@outlook.com>
2022-12-06 15:02:24 +0800

    add database models

--- django_be/hoimsystem/models/doctorOp.py
+++ django_be/hoimsystem/models/doctorOp.py
@@ -2,3 +2,11 @@ from django.db import models
 import uuid
 from django.db import models
 # Create your models here.
+
+# 处方
+class prescription(models.Model):
+  prescription_id = models.UUIDField(primary_key=True)
+
+# 病历
+class medical_record(models.Model):
+  medical_record_id = models.UUIDField(primary_key=True)

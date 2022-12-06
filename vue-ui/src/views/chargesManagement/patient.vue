118f7b947b6e286c09dd56513a66b0d838446002
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 15:01:26 +0800

    更新收费记录查看页面

---
+++ vue-ui/src/views/chargesManagement/patient.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      :element-loading-text="elementLoadingText"
+    >
+      <el-table-column
+        show-overflow-tooltip
+        prop="id"
+        label="id"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="收费日期"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="收费时间"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="具体项目"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="状态"
+      ></el-table-column>
+      <el-table-column show-overflow-tooltip label="操作" width="200">
+        <template #default="{ row }">
+          <el-button type="text" @click="handleEdit(row)">付款</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ChargesPatient',
+  }
+</script>

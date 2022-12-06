c99e9460550469a36df1d25402a671379a568687
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 15:02:04 +0800

    添加科室注册页面

---
+++ vue-ui/src/views/departmentManagement/departmentRegister.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-form ref="form" :model="form" label-width="124px">
+      <el-form-item label="科室名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="科室主任">
+        <el-input v-model="form.director"></el-input>
+      </el-form-item>
+      <el-form-item label="科室联系方式">
+        <el-input v-model="form.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="科室位置">
+        <el-input v-model="form.location"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">注册</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DepartmentRegister',
+  }
+</script>

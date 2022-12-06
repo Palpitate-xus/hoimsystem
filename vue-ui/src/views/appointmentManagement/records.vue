3dd57a7eeecd5f6945b9f396abd9776cbb7750b3
Palpitate-xus <xus1758011080@outlook.com>
2022-12-06 14:36:27 +0800

    更新病人查看预约记录的页面

--- vue-ui/src/views/appointmentManagement/records.vue
+++ vue-ui/src/views/appointmentManagement/records.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    <h1>预约记录查看</h1>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      :element-loading-text="elementLoadingText"
+    >
+      <el-table-column
+        show-overflow-tooltip
+        prop="id"
+        label="id"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="医生"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="时间段"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="类别"
+      ></el-table-column>
+      <el-table-column show-overflow-tooltip label="操作" width="200">
+        <template #default="{ row }">
+          <el-button type="text" @click="handleEdit(row)">取消预约</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 

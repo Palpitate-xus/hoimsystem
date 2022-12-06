ed47a8e0269995275f608ae5bc25f6e329d1bb77
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 13:57:29 +0800

    完成医生注册api

--- django_be/hoimsystem/views/doctorManagement.py
+++ django_be/hoimsystem/views/doctorManagement.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 import json
 from hoimsystem.models.roleUser import *
+from hoimsystem.models.adminOp import *
 # Create your views here.
 
 # 测试api
@@ -9,6 +10,7 @@ def test(request):
     response = {"code": 200, "msg": 'success'}
     return HttpResponse(json.dumps(response))
 
+# 医生注册
 def add_doctor(request):
     received_data = json.loads(request.body.decode())
     username = received_data.get('username')
@@ -17,11 +19,12 @@ def add_doctor(request):
     title = received_data.get('title')
     sex = received_data.get('sex')
     phone = received_data.get('phone')
-    department = received_data.get('department')
+    department_in = department.objects.get(name=received_data.get('department'))
     permission = received_data('permission')
     education = received_data.get('education')
     try:
-        doctor.objects.create(name=name, sex=sex, title=title, education=education, phone=phone, permission=permission)
+        # 在医生表中插入记录
+        doctor.objects.create(name=name, sex=sex, title=title, education=education, phone=phone, permission=permission, department_id=department_in)
         # 判断是否为科室主任
         if permission == 'director':
             users.objects.create(username=username, password=password, user_role="director")

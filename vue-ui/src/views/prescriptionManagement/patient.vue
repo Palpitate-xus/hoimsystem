c3c687d8f2d790f14635f201b913b7053aae5602
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-06 14:37:19 +0800

    更新病人查看处方的页面

--- vue-ui/src/views/prescriptionManagement/patient.vue
+++ vue-ui/src/views/prescriptionManagement/patient.vue
@@ -1,9 +1,35 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      :element-loading-text="elementLoadingText"
+    >
+      <el-table-column
+        show-overflow-tooltip
+        prop="id"
+        label="id"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="医生"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="时间段"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="username"
+        label="类别"
+      ></el-table-column>
+      <el-table-column show-overflow-tooltip label="操作" width="200">
+        <template #default="{ row }">
+          <el-button type="text" @click="handleEdit(row)">取消预约</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>

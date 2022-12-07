e5bd96958c87c5b41cfcf281f1a949a9a65a1350
Palpitate-xus <xus1758011080@outlook.com>
2022-12-07 21:11:57 +0800

    add pharmaceuticalManagement api

--- vue-ui/src/api/pharmaceuticalManagement.js
+++ vue-ui/src/api/pharmaceuticalManagement.js
@@ -1,1 +1,17 @@
 import request from '@/utils/request'
+
+export function getPharmaceuticalList(data) {
+  return request({
+    url: '/pharmaceuticalManagement/getList',
+    method: 'post',
+    data,
+  })
+}
+
+export function pharmaceuticalRegister(data) {
+  return request({
+    url: '/pharmaceuticalManagement/create',
+    method: 'post',
+    data,
+  })
+}

779ae01fd74f3f792b0182607e622081a6a14cf8
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-07 13:23:27 +0800

    add 预约就诊时间段管理

--- vue-ui/src/views/admin/settings.vue
+++ vue-ui/src/views/admin/settings.vue
@@ -10,7 +10,7 @@
           <el-button type="primary" @click="onSubmit">立即创建</el-button>
         </el-row>
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="预约就诊时间段调整">
         <el-select v-model="form.region" placeholder="请选择活动区域">
           <el-option label="区域一" value="shanghai"></el-option>
           <el-option label="区域二" value="beijing"></el-option>

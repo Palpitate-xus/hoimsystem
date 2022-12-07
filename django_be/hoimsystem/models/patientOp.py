dcbe9f1bc43e2313f917701e8abf82e8644bc4f3
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-07 21:12:53 +0800

    update patient models

--- django_be/hoimsystem/models/patientOp.py
+++ django_be/hoimsystem/models/patientOp.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # 挂号
 class registration(models.Model):
-  registration_uuid = models.UUIDField()  # 挂号id
+  registration_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # 挂号id
   registration_id = models.IntegerField()  # 就诊顺序
   patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)  # 病人id
   doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)  # 医生id
@@ -15,16 +15,16 @@ class registration(models.Model):
 
 # 预约
 class appointment(models.Model):
-  registration_uuid = models.UUIDField()
-  patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)
-  doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
-  category = models.ForeignKey('reg_category', on_delete=models.PROTECT)
-  department_id = models.ForeignKey('department', on_delete=models.CASCADE)
-  prefer_time = models.ForeignKey
-  status = models.IntegerField()
+  registration_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # 预约id
+  patient_id = models.ForeignKey('patient', on_delete=models.CASCADE)  # 病人id
+  doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)  # 医生id
+  category = models.ForeignKey('reg_category', on_delete=models.PROTECT)  # 挂号类别
+  department_id = models.ForeignKey('department', on_delete=models.CASCADE)  # 科室id
+  prefer_time = models.ForeignKey('timeslot', on_delete=models.PROTECT)  # 预约时间段
+  status = models.IntegerField()  # 预约状态
 
 
 # 预约违约记录
 class breach_record(models.Model):
-  breach_id = models.UUIDField()
+  breach_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # 违约记录id
   registration_id = models.ForeignKey('registration', on_delete=models.CASCADE)

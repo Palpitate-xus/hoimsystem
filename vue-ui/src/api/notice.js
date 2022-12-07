02ddad72120390b539a282e66146f095dc740518
Palpitate-xus <xus1758011080@outlook.com>
2022-12-07 15:08:16 +0800

    bug fix

--- vue-ui/src/api/notice.js
+++ vue-ui/src/api/notice.js
@@ -1,8 +1,17 @@
 import request from '@/utils/request'
 
-export function getNoticeList() {
+export function getNoticeList(data) {
   return request({
     url: '/notice/getList',
     method: 'post',
+    data,
+  })
+}
+
+export function noticeRegister(data) {
+  return request({
+    url: '/notice/create',
+    method: 'post',
+    data,
   })
 }

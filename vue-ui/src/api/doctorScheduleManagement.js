25972f1cb4aee3c86a4b962ee3f2b68e4f4b3ae5
Palpitate-xus <xus1758011080@outlook.com>
2022-12-07 21:11:38 +0800

    add doctorScheduleManagement api

--- vue-ui/src/api/doctorScheduleManagement.js
+++ vue-ui/src/api/doctorScheduleManagement.js
@@ -1,1 +1,17 @@
 import request from '@/utils/request'
+
+export function getDoctorScheduleList(data) {
+  return request({
+    url: '/doctorScheduleManagement/getList',
+    method: 'post',
+    data,
+  })
+}
+
+export function doctorScheduleRegister(data) {
+  return request({
+    url: '/doctorScheduleManagement/register',
+    method: 'post',
+    data,
+  })
+}

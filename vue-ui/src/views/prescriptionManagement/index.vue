21fb6a2a130873257d45c1f389fdd2500bdf6d47
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-08 22:26:13 +0800

    update prescriptionManagement for admin

--- vue-ui/src/views/prescriptionManagement/index.vue
+++ vue-ui/src/views/prescriptionManagement/index.vue
@@ -1,31 +1,90 @@
 <template>
-  <div>
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+  <div class="test-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      :element-loading-text="elementLoadingText"
+      @selection-change="setSelectRows"
+    >
+      <el-table-column show-overflow-tooltip type="selection"></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="uuid"
+        label="uuid"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="patient"
+        label="病人"
+      ></el-table-column>
+      <el-table-column
+        v-permissions="['admin']"
+        show-overflow-tooltip
+        prop="doctor"
+        label="医生"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="phas"
+        label="内容"
+      ></el-table-column>
     </el-table>
   </div>
 </template>
-
 <script>
+  import { getPrescriptionList } from '../../api/prescriptionManagement'
   export default {
-    name: 'PresciptionManagement',
+    name: 'Index',
     data() {
       return {
-        tableData: [
-          {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄',
-          },
-        ],
+        list: null,
+        listLoading: true,
+        total: 0,
+        selectRows: '',
+        elementLoadingText: '正在加载...',
       }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      setSelectRows(val) {
+        this.selectRows = val
+      },
+      handleEdit(row) {
+        if (row.id) {
+          this.$refs['edit'].showEdit(row)
+        } else {
+          this.$refs['edit'].showEdit()
+        }
+      },
+      handleDelete(row) {
+        if (row.id) {
+          this.$baseConfirm('你确定要删除当前项吗', null, async () => {
+            const { msg } = await doDelete({ ids: row.id })
+            this.$baseMessage(msg, 'success')
+            this.fetchData()
+          })
+        } else {
+          if (this.selectRows.length > 0) {
+            const ids = this.selectRows.map((item) => item.id).join()
+            this.$baseConfirm('你确定要删除选中项吗', null, async () => {
+              const { msg } = await doDelete({ ids })
+              this.$baseMessage(msg, 'success')
+              this.fetchData()
+            })
+          } else {
+            this.$baseMessage('未选中任何行', 'error')
+            return false
+          }
+        }
+      },
+      async fetchData() {
+        this.listLoading = true
+        const { data } = await getPrescriptionList()
+        this.list = data
+        this.listLoading = false
+      },
     },
   }
 </script>

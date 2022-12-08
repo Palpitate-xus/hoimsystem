36751f8e28303e6ebf56092feaff7ae685fac089
Palpitate-xus <xus1758011080@outlook.com>
2022-12-08 22:25:51 +0800

    update prescriptionManagement for doctor

---
+++ vue-ui/src/views/prescriptionManagement/doctor.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <el-form ref="form" :model="form" label-width="124px">
+      <el-form-item label="选择病人">
+        <el-select v-model="form.patient" placeholder="请选择病人">
+          <el-option
+            v-for="item in patientList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="选择药品">
+        <el-row :gutter="8">
+          <el-col :span="4">
+            <el-select v-model="pha_choose" placeholder="请选择药品">
+              <el-option
+                v-for="item in phaList"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="4">
+            <el-input
+              v-model="pha_number"
+              placeholder="请输入药品数量"
+            ></el-input>
+          </el-col>
+          <el-col :span="8">
+            <el-button @click="addPha">加入</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
+    <el-table
+      v-loading="listLoading"
+      :data="pha_submit"
+      :element-loading-text="elementLoadingText"
+    >
+      <el-table-column
+        show-overflow-tooltip
+        prop="id"
+        label="id"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="name"
+        label="药品名称"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="number"
+        label="数量"
+      ></el-table-column>
+      <el-table-column show-overflow-tooltip label="操作" width="200">
+        <template #default="{ row }">
+          <el-button type="text" @click="handleDelete(row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-button
+      style="margin-top: 10px; text-align: center"
+      type="primary"
+      @click="onSubmit"
+    >
+      提交
+    </el-button>
+    <el-button @click="cancel">取消</el-button>
+  </div>
+</template>
+
+<script>
+  import { getPatientList } from '@/api/patientManagement'
+  import { getPharmaceuticalList } from '../../api/pharmaceuticalManagement'
+  import { prescriptionRegister } from '../../api/prescriptionManagement'
+  export default {
+    name: 'PrescriptionRegister',
+    data() {
+      return {
+        form: {
+          patient: 1,
+          phas: [],
+        },
+        patientList: [],
+        phaList: [],
+        pha_submit: [],
+        pha_choose: null,
+        pha_number: null,
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      async fetchData() {
+        const patient_data = await getPatientList()
+        this.patientList = patient_data.data
+        const pha_data = await getPharmaceuticalList()
+        this.phaList = pha_data.data
+      },
+      async onSubmit() {
+        console.log('submit!')
+        this.form.phas = this.pha_submit
+        await prescriptionRegister(this.form)
+        this.$baseMessage('提交成功', 'success')
+        this.cancel()
+      },
+      addPha() {
+        let array = this.pha_submit
+        let flag = 1
+        for (let index = 0; index < array.length; index++) {
+          if (array[index].id === this.pha_choose) {
+            this.$baseMessage('已有重复项目', 'error')
+            flag = 0
+          }
+        }
+        let pha_name = ''
+        array = this.phaList
+        for (let index = 0; index < array.length; index++) {
+          if (array[index].id === this.pha_choose) {
+            pha_name = array[index].name
+            console.log(pha_name)
+          }
+        }
+        if (flag) {
+          this.pha_submit.push({
+            id: this.pha_choose,
+            name: pha_name,
+            number: this.pha_number,
+          })
+        }
+      },
+      handleDelete(row) {
+        this.pha_submit.map((val, i) => {
+          if (val.id === row.id) {
+            this.pha_submit.splice(i, 1)
+            console.log(this.pha_submit)
+          }
+        })
+      },
+      cancel() {
+        this.form = {
+          patient: 1,
+        }
+      },
+    },
+  }
+</script>

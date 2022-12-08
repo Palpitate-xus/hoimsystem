4ea3eb3d5a97e41a17c9e70181c7cea4ba87696e
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-08 22:23:41 +0800

    add charge api

--- django_be/hoimsystem/views/patientManagement.py
+++ django_be/hoimsystem/views/patientManagement.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.utils import timezone
 import json
+from hoimsystem.models.roleUser import *
+from hoimsystem.models.patientOp import *
+from hoimsystem.models.doctorOp import *
 # Create your views here.
 
 # 测试api
@@ -56,7 +60,34 @@ def get_medicalrecords_list(request):
 
 # 收费记录查看
 def get_charges_list(request):
-    response = {"code": 200, "msg": 'success'}
+    role = users.objects.get(username=request.META.get('HTTP_ACCESSTOKEN')).user_role
+    username = users.objects.get(username=request.META.get('HTTP_ACCESSTOKEN')).username
+    data = []
+    if role == 'admin':
+        charge_list = charge.objects.all()
+        for item in charge_list:
+            data.append({
+                'id': str(item.charge_id),
+                'charge_time': str(item.charge_time),
+                'time': str(item.time),
+                'pre_id': str(item.prescription_id.prescription_id),
+                'amount': item.amount,
+                'status': item.status,
+            })
+    elif role == 'patient':
+        patient_id = patient.objects.get(identity=username).patient_id
+        pres = prescription.objects.filter(patient_id=patient_id)
+        for pre in pres:
+            item = charge.objects.get(prescription_id=pre.prescription_id)
+            data.append({
+                'id': str(item.charge_id),
+                'charge_time': str(item.charge_time),
+                'time': str(item.time),
+                'pre_id': str(item.prescription_id.prescription_id),
+                'amount': item.amount,
+                'status': item.status,
+            })
+    response = {"code": 200, "msg": 'success', "data": data}
     return HttpResponse(json.dumps(response))
 
 

fabd6860cfa44cd9b968c561d3199aab0c7217e6
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-08 22:25:05 +0800

    update appointmentManagement for patient

--- vue-ui/src/views/appointmentManagement/records.vue
+++ vue-ui/src/views/appointmentManagement/records.vue
@@ -37,5 +37,56 @@
 <script>
   export default {
     name: 'AppointmentRecords',
+    data() {
+      return {
+        list: null,
+        listLoading: true,
+        total: 0,
+        selectRows: '',
+        elementLoadingText: '正在加载...',
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      setSelectRows(val) {
+        this.selectRows = val
+      },
+      handleEdit(row) {
+        if (row.id) {
+          this.$refs['edit'].showEdit(row)
+        } else {
+          this.$refs['edit'].showEdit()
+        }
+      },
+      handleDelete(row) {
+        if (row.id) {
+          this.$baseConfirm('你确定要删除当前项吗', null, async () => {
+            const { msg } = await doDelete({ ids: row.id })
+            this.$baseMessage(msg, 'success')
+            this.fetchData()
+          })
+        } else {
+          if (this.selectRows.length > 0) {
+            const ids = this.selectRows.map((item) => item.id).join()
+            this.$baseConfirm('你确定要删除选中项吗', null, async () => {
+              const { msg } = await doDelete({ ids })
+              this.$baseMessage(msg, 'success')
+              this.fetchData()
+            })
+          } else {
+            this.$baseMessage('未选中任何行', 'error')
+            return false
+          }
+        }
+      },
+      async fetchData() {
+        this.listLoading = true
+        const { data } = await getDoctorList()
+        this.list = data
+        this.listLoading = false
+      },
+    },
   }
 </script>

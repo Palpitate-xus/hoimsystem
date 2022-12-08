0efac55446aced72618322525a822dcd8fd7dfd1
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-08 22:23:50 +0800

    add apis

--- vue-ui/src/api/chargesManagement.js
+++ vue-ui/src/api/chargesManagement.js
@@ -1,1 +1,9 @@
 import request from '@/utils/request'
+
+export function getChargeList(data) {
+  return request({
+    url: '/chargeManagement/getList',
+    method: 'post',
+    data,
+  })
+}
--- vue-ui/src/api/prescriptionManagement.js
+++ vue-ui/src/api/prescriptionManagement.js
@@ -1,1 +1,17 @@
 import request from '@/utils/request'
+
+export function getPrescriptionList(data) {
+  return request({
+    url: '/prescriptionManagement/getList',
+    method: 'post',
+    data,
+  })
+}
+
+export function prescriptionRegister(data) {
+  return request({
+    url: '/prescriptionManagement/create',
+    method: 'post',
+    data,
+  })
+}

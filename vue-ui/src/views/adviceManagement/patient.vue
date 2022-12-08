70bbfb84dbefc8f7ff79f9cf5bae3201d2ca08e7
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-08 22:24:48 +0800

    update adviceManagement for patient

--- vue-ui/src/views/adviceManagement/patient.vue
+++ vue-ui/src/views/adviceManagement/patient.vue
@@ -7,22 +7,22 @@
     >
       <el-table-column
         show-overflow-tooltip
-        prop="id"
-        label="id"
+        prop="uuid"
+        label="uuid"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
-        label="医生"
-      ></el-table-column>
-      <el-table-column
-        show-overflow-tooltip
-        prop="username"
+        prop="department"
         label="科室"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="doctor"
+        label="医生"
+      ></el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="content"
         label="内容"
       ></el-table-column>
     </el-table>
@@ -32,5 +32,56 @@
 <script>
   export default {
     name: 'AdviceManagement',
+    data() {
+      return {
+        list: null,
+        listLoading: true,
+        total: 0,
+        selectRows: '',
+        elementLoadingText: '正在加载...',
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      setSelectRows(val) {
+        this.selectRows = val
+      },
+      handleEdit(row) {
+        if (row.id) {
+          this.$refs['edit'].showEdit(row)
+        } else {
+          this.$refs['edit'].showEdit()
+        }
+      },
+      handleDelete(row) {
+        if (row.id) {
+          this.$baseConfirm('你确定要删除当前项吗', null, async () => {
+            const { msg } = await doDelete({ ids: row.id })
+            this.$baseMessage(msg, 'success')
+            this.fetchData()
+          })
+        } else {
+          if (this.selectRows.length > 0) {
+            const ids = this.selectRows.map((item) => item.id).join()
+            this.$baseConfirm('你确定要删除选中项吗', null, async () => {
+              const { msg } = await doDelete({ ids })
+              this.$baseMessage(msg, 'success')
+              this.fetchData()
+            })
+          } else {
+            this.$baseMessage('未选中任何行', 'error')
+            return false
+          }
+        }
+      },
+      async fetchData() {
+        this.listLoading = true
+        const { data } = await getDoctorList()
+        this.list = data
+        this.listLoading = false
+      },
+    },
   }
 </script>

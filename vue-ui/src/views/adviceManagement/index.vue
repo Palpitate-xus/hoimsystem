1e15304ed2502d075aae34a1b9f346a1749d40e4
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-09 20:03:01 +0800

    add adviceRegister page

--- vue-ui/src/views/adviceManagement/index.vue
+++ vue-ui/src/views/adviceManagement/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <h1>测试</h1>
+    <el-form ref="form" :model="form" label-width="140px">
+      <el-form-item label="病人选择">
+        <el-select v-model="form.department" placeholder="请选择病人">
+          <el-option
+            v-for="item in departmentList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="医嘱内容">
+        <el-input
+          v-model="form.content"
+          type="textarea"
+          :autosize="{ minRows: 4, maxRows: 8 }"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查找</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 

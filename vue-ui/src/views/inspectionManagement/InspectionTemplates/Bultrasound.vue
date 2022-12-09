cf93af474df349a24bdfbaf8ee910acbaa698113
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-09 20:04:25 +0800

    测试检查模板页面

--- vue-ui/src/views/inspectionManagement/InspectionTemplates/Bultrasound.vue
+++ vue-ui/src/views/inspectionManagement/InspectionTemplates/Bultrasound.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <h1>检查模板_B超</h1>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          确 定
+        </el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 

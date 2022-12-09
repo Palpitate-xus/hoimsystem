9c9d91d3a4e9645d38a00bb374f336dfef849cdf
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-09 12:51:03 +0800

    优化挂号页面

--- vue-ui/src/views/registrationManagement/index.vue
+++ vue-ui/src/views/registrationManagement/index.vue
@@ -1,43 +1,5 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="140px">
-      <el-form-item label="科室选择">
-        <el-select v-model="form.department" placeholder="请选择科室">
-          <el-option
-            v-for="item in departmentList"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="医生姓名">
-        <el-select v-model="form.doctor" placeholder="请选择医生">
-          <el-option
-            v-for="item in doctorList"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="就诊时间段">
-        <el-select v-model="form.time" placeholder="请选择就诊时间段">
-          <el-option
-            v-for="item in timeSlotList"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="是否专家号">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查找</el-button>
-      </el-form-item>
-    </el-form>
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -78,13 +40,14 @@
 </template>
 
 <script>
-  import { getDepartmentList } from '../../api/departmentManagement'
-  import { getDoctorList } from '../../api/doctorManagement'
+  import { getDoctorScheduleList } from '@/api/doctorScheduleManagement'
   export default {
     name: 'RegistrationManagement',
     data() {
       return {
-        form: {},
+        form: {
+          specialist: 0,
+        },
         departmentList: [],
         doctorList: [],
         timeSlotList: [],
@@ -95,10 +58,8 @@
     },
     methods: {
       async fetchData() {
-        const data_department = await getDepartmentList()
-        const data_doctor = await getDoctorList()
-        this.departmentList = data_department.data
-        this.doctorList = data_doctor.data
+        const { data } = await getDoctorScheduleList()
+        console.log(data)
       },
       onSubmit() {
         console.log('submit!')

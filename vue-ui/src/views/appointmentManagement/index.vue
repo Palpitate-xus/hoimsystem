04b00018acd249d2ce991e0d832c6d71c9afe8d8
Palpitate-xus <xus1758011080@outlook.com>
2022-12-09 20:03:22 +0800

    优化预约逻辑

--- vue-ui/src/views/appointmentManagement/index.vue
+++ vue-ui/src/views/appointmentManagement/index.vue
@@ -1,48 +1,5 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="140px">
-      <el-form-item label="科室选择">
-        <el-select v-model="form.department" placeholder="请选择科室">
-          <el-option
-            v-for="item in departmentList"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="医生姓名">
-        <el-select v-model="form.doctor" placeholder="请选择医生">
-          <el-option
-            v-for="item in doctorList"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="预约日期">
-        <el-date-picker
-          v-model="form.date"
-          align="right"
-          type="date"
-          placeholder="选择日期"
-          :picker-options="pickerOptions"
-        ></el-date-picker>
-      </el-form-item>
-      <el-form-item label="就诊时间段">
-        <el-select v-model="form.region" placeholder="请选择就诊时间段">
-          <el-option label="上午" value="shanghai"></el-option>
-          <el-option label="下午" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="是否专家号">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查找</el-button>
-      </el-form-item>
-    </el-form>
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -55,27 +12,27 @@
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="date"
         label="日期"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="time"
         label="时间段"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="doctor"
         label="医生"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="specialist"
         label="类别"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="email"
+        prop="available"
         label="剩余号源数量"
       ></el-table-column>
       <el-table-column show-overflow-tooltip label="操作" width="200">

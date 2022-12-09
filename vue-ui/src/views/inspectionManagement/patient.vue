bdbe9c28fbead8f913d01f386fc46e5b48bc97a7
Palpitate-xus <xus1758011080@outlook.com>
2022-12-09 20:04:12 +0800

    更新病人查看检查结果页面

--- vue-ui/src/views/inspectionManagement/patient.vue
+++ vue-ui/src/views/inspectionManagement/patient.vue
@@ -12,24 +12,24 @@
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="doctor"
         label="医生"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
-        label="时间段"
+        prop="inspection"
+        label="检查名称"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
-        label="类别"
+        prop="time"
+        label="检查时间"
       ></el-table-column>
-      <el-table-column show-overflow-tooltip label="操作" width="200">
-        <template #default="{ row }">
-          <el-button type="text" @click="handleEdit(row)">取消预约</el-button>
-        </template>
-      </el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="result"
+        label="检查结果"
+      ></el-table-column>
     </el-table>
   </div>
 </template>

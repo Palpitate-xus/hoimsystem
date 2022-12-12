29232049e87fef640c8cc08722871d65e0cd7f21
Palpitate-xus <palpitate.xus@outlook.com>
2022-12-12 17:51:50 +0800

    更新管理员主页

--- vue-ui/src/views/admin/index.vue
+++ vue-ui/src/views/admin/index.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="magnifier-container">
-    <h1>这是管理员页面</h1>
     <el-row>
       <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
-        <el-card>待办事项</el-card>
+        <el-card>当前就诊数</el-card>
       </el-col>
       <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
         <el-card>当前设置</el-card>

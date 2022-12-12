31860067c7f0a67cdd1fc1864974eae0502d24ab
Palpitate-xus <xus1758011080@outlook.com>
2022-12-12 17:51:40 +0800

    添加挂号与预约后端处理逻辑

--- django_be/hoimsystem/views/patientManagement.py
+++ django_be/hoimsystem/views/patientManagement.py
@@ -6,6 +6,7 @@ import json
 from hoimsystem.models.roleUser import *
 from hoimsystem.models.patientOp import *
 from hoimsystem.models.doctorOp import *
+from hoimsystem.models.adminOp import *
 # Create your views here.
 
 # 测试api
@@ -15,26 +16,72 @@ def test(request):
 
 # 挂号信息
 def registrationList(request):
-    data = []
     today_weeky = datetime.now().weekday()
-    print(today_weeky)
+    weekdays = ['星期一', '星期二', '星期三', '星期四', '星期五']
+    if today_weeky > 4:
+        data = '今天为休息日'
+    else:
+        data = []
+        schedules = doctor_schedule.objects.filter(week=weekdays[today_weeky])
+        for item in schedules:
+            data.append({
+                'id': item.schedule_id,
+                'time': item.time,
+                'specialist': item.specialist,
+                'time': item.time,
+                'doctor': item.doctor_id.name,
+                'doctor_id': item.doctor_id.doctor_id,
+                'department_id': item.doctor_id.department_id.department_id,
+            })
     response = {"code": 200, "msg": 'success', "data": data}
     return HttpResponse(json.dumps(response))
 
 # 病人挂号
 def patient_registration(request):
+    token = users.objects.get(username=request.META.get('HTTP_ACCESSTOKEN')).username
+    patient_id = patient.objects.get(identity=token).patient_id
     received_data = json.loads(request.body.decode())
+    registration_id = 1
+    doctor_id = received_data.get('doctor_id')
+    specialist = received_data.get('specialist')
+    time = timezone.now()
+    status = 0
+    department_id = received_data.get('department_id')
+    registration.objects.create(registration_id=registration_id, patient_id=patient_id, doctor_id=doctor_id, specialist=specialist, department_id=department_id, time=time, status=status)
     response = {"code": 200, "msg": 'success'}
     return HttpResponse(json.dumps(response))
 
 # 病人取消挂号
 def patient_registration_cancel(request):
     received_data = json.loads(request.body.decode())
+    uuid = received_data.get('uuid')
+    registration_obj = registration.objects.get(registration_uuid=uuid)
+    registration_obj.status=3
+    registration_obj.save()
     response = {"code": 200, "msg": 'success'}
     return HttpResponse(json.dumps(response))
 
 # 挂号记录查看
 def get_registration_list(request):
+    token = users.objects.get(username=request.META.get('HTTP_ACCESSTOKEN')).username
+    patient_id = patient.objects.get(identity=token).patient_id
+    registration_list = registration.objects.filter(patient_id=patient_id)
+    data = []
+    for item in registration_list:
+        data.append({
+            'uuid': item.registration_uuid,
+            'order': item.registration_id,
+            'doctor': item.doctor_id.name,
+            'specialist': item.specialist,
+            'department': item.department_id.name,
+            'time': item.time,
+            'status': item.status,
+        })
+    response = {"code": 200, "msg": 'success', "data": data}
+    return HttpResponse(json.dumps(response))
+
+# 预约信息
+def appointmentList(request):
     response = {"code": 200, "msg": 'success'}
     return HttpResponse(json.dumps(response))
 
@@ -52,7 +99,21 @@ def patient_appointment_cancel(request):
 
 # 预约记录查看
 def get_appointment_list(request):
-    response = {"code": 200, "msg": 'success'}
+    token = users.objects.get(username=request.META.get('HTTP_ACCESSTOKEN')).username
+    patient_id = patient.objects.get(identity=token).patient_id
+    appointment_list = appointment.objects.filter(patient_id=patient_id)
+    data = []
+    for item in appointment_list:
+        data.append({
+            'uuid': item.registration_uuid,
+            'order': item.registration_id,
+            'doctor': item.doctor_id.name,
+            'specialist': item.specialist,
+            'department': item.department_id.name,
+            'time': item.time,
+            'status': item.status,
+        })
+    response = {"code": 200, "msg": 'success', "data": data}
     return HttpResponse(json.dumps(response))
 
 # 检查记录查看

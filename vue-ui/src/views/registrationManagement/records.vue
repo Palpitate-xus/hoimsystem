ab32462b1bc0cb93741a2d07f386948cb5199643
Palpitate-xus <xus1758011080@outlook.com>
2022-12-12 21:45:48 +0800

    更新挂号管理页面

--- vue-ui/src/views/registrationManagement/records.vue
+++ vue-ui/src/views/registrationManagement/records.vue
@@ -7,27 +7,54 @@
     >
       <el-table-column
         show-overflow-tooltip
-        prop="id"
-        label="id"
+        prop="uuid"
+        label="uuid"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="doctor"
         label="医生"
       ></el-table-column>
+      <el-table-column show-overflow-tooltip prop="time" label="日期">
+        <template slot-scope="scope">
+          {{ scope.row.time }}
+        </template>
+      </el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
-        label="日期"
+        prop="order"
+        label="排队序号"
       ></el-table-column>
       <el-table-column
         show-overflow-tooltip
-        prop="username"
+        prop="specialist"
         label="类别"
-      ></el-table-column>
+        sortable
+      >
+        <template slot-scope="scope">
+          <el-tag
+            :type="scope.row.specialist === 1 ? 'warning' : 'info'"
+            disable-transitions
+          >
+            {{ scope.row.specialist === 1 ? '专家号' : '普通号' }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column
+        show-overflow-tooltip
+        prop="status"
+        label="状态"
+        sortable
+      >
+        <template slot-scope="scope">
+          <el-tag :type="success">
+            {{ scope.row.status }}
+          </el-tag>
+        </template>
+      </el-table-column>
       <el-table-column show-overflow-tooltip label="操作" width="200">
         <template #default="{ row }">
-          <el-button type="text" @click="handleEdit(row)">取消预约</el-button>
+          <el-button type="text" @click="handleCancel(row)">取消挂号</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -35,8 +62,37 @@
 </template>
 
 <script>
-  import { cancelRegistration } from '@/api/registrationManagement'
+  import {
+    cancelRegistration,
+    getRegistrationList,
+  } from '@/api/registrationManagement'
   export default {
     name: 'RegistrationRecords',
+    data() {
+      return {
+        form: {
+          specialist: 0,
+        },
+        list: [],
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      async fetchData() {
+        const { data } = await getRegistrationList()
+        this.list = data
+        console.log(data)
+      },
+      async handleCancel(row) {
+        if (row.status === '已取消') {
+          this.$baseMessage('不能取消已取消的挂号', 'warning')
+        } else {
+          await cancelRegistration(row)
+          this.$baseMessage('取消成功', 'success')
+        }
+      },
+    },
   }
 </script>
